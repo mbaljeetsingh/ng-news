feca3d3161b5d326cf1fa290b794a2becd36d396
Baljeet Singh <bsinghs.007@gmail.com>
2018-11-09 16:09:55 +0530

    add news items layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  news = new Array(20);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
