2bab10f99d9a10ebf5e8e6675fc783af5a5938f1
Baljeet Singh <bsinghs.007@gmail.com>
2018-11-09 14:25:49 +0530

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { HomeComponent } from './home/home.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
+import { SourcesComponent } from './sources/sources.component';
+import { FavoritesComponent } from './favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeadlinesComponent,
+    SourcesComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
