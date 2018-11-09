79eedeaaa53464cac9c1e6804157508887f34227
Baljeet Singh <bsinghs.007@gmail.com>
2018-11-09 15:50:09 +0530

    adding routes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatToolbarModule],
-  exports: [MatButtonModule, MatToolbarModule]
+  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatCardModule],
+  exports: [MatButtonModule, MatToolbarModule, MatCardModule]
 })
 export class MaterialModule {}
