4b6029ec9f32dca954d50e0d43b0119e5de8f080
Baljeet Singh <bsinghs.007@gmail.com>
2018-12-12 17:06:58 +0530

    updating headlines

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatCardModule],
-  exports: [MatButtonModule, MatToolbarModule, MatCardModule]
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatChipsModule
+  ],
+  exports: [MatButtonModule, MatToolbarModule, MatCardModule, MatChipsModule]
 })
 export class MaterialModule {}
