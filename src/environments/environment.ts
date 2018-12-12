363ad0cfa89ea1e1d96421f67e0601e22d7ee5aa
Baljeet Singh <bsinghs.007@gmail.com>
2018-12-12 14:03:04 +0530

    update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '7b5d0d2f240b41759f0ab56f9ddb06c7',
+  apiUrl: 'https://newsapi.org/v2'
 };
 
 /*
